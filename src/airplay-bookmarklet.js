ee9acadc74a65b8f199035aac280837d58459a6d
Janroel Koppen <j.koppen@headsfirst.nl>
2019-08-09 21:08:24 +0200

    default to first, prefer first playing

--- src/airplay-bookmarklet.js
+++ src/airplay-bookmarklet.js
@@ -18,21 +18,13 @@ for (i = 0; i < audios.length; i++) {
 		avs.push(audio);
 	}
 }
-if (avs.length > 0) {
-	var select = document.createElement('select');
-	var option = document.createElement('option');
-	select.onchange = function(ev){ var i = this.options[this.selectedIndex].value; avs[i].setAttribute('preload','metadata'); avs[i].setAttribute('x-webkit-airplay','allow'); avs[i].webkitShowPlaybackTargetPicker(); };
-	select.setAttribute('style', "z-index:999999");
-	var option = document.createElement('option');
-	option.setAttribute('value', "-1");
-	option.textContent = "[select media]";
-	select.appendChild(option);
-	for (i = 0; i < avs.length; i++) {
-		var option = document.createElement('option');
-		option.setAttribute('value', i);
-		option.textContent = (avs[i].src) ? avs[i].src.substr(0,32) : 'unknown';
-		select.appendChild(option);
-	}
-	document.body.insertBefore(select, document.body.firstChild)
+var av = avs[0]; /* default to first */
+for (i = 0; i < avs.length; i++) {
+	if (!avs[i].paused) { av = avs[i]; break; } /* prefer first playing */
+}
+if (av && av.webkitShowPlaybackTargetPicker) { 
+	av.setAttribute('preload','metadata');
+	av.setAttribute('x-webkit-airplay','allow');
+	av.webkitShowPlaybackTargetPicker();
 }
 }());
